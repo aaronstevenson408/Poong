91e48464b609e6a37ff95627dd3c5fed493a1a51
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2023-03-08 20:45:47 -0800

    Update Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public Rigidbody2D rb2d;
+    public float maxInitialAngle = 0.66f;
+    public float moveSpeed = 1.0f;
+
 
     void Start()
     {
-        rb2d.velocity = Vector2.left;
+        Vector2 dir = Vector2.left;
+        dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);    
+        rb2d.velocity = dir * moveSpeed;
     }
 }
