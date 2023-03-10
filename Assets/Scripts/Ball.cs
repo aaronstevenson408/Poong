d5626db570393f42ba62b207197d6f6f37a2c4cb
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2023-03-09 22:23:40 -0800

    working on ui

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -7,15 +8,23 @@ public class Ball : MonoBehaviour
     public Rigidbody2D rb2d;
     public float maxInitialAngle = 0.66f;
     public float moveSpeed = 1.0f;
+    public float maxYStart = 3.5f;
+    public float startX = 0;
 
 
     void Start()
     {
         InitialPush();
     }
+    private void ResetBall()
+    {
+        float posY = Random.Range( -maxYStart, maxYStart );
+        Vector2 position = new Vector2(startX, posY);
+        transform.position = position;
+    }
     private void InitialPush()
     {
-        Vector2 dir = Vector2.left;
+        Vector2 dir = Random.value < 0.5f ? Vector2.right : Vector2.left;
         dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
         rb2d.velocity = dir * moveSpeed;
     }
@@ -23,7 +32,8 @@ public class Ball : MonoBehaviour
     {
         ScoreZone scoreZone = collision.GetComponent<ScoreZone>();  
         if (scoreZone) {
-            Debug.Log("hit Score Zone");
+            ResetBall();
+            InitialPush();
         }
     }
 }
