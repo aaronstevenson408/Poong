8a8c06410f53a8109282c1a34ac4da84ed67bf76
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2023-03-12 13:15:52 -0700

    finished setting up paddles , working on paddle scripts

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float id;
+    public float moveSpeed = 2.0f;
+
+}
