8ed1fe706d8fce5dd0b05edabd759a77c2577005
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2023-03-12 13:43:23 -0700

    finished paddles , technically complete pong

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,7 +5,38 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     public Rigidbody2D rb2d;
-    public float id;
+    public int id;
     public float moveSpeed = 2.0f;
 
+    private void Update()
+    {
+        float movement = ProcessInput();
+        Move(movement);
+    }
+    private float ProcessInput() 
+    {
+        float movement = 0f;
+
+        switch (id)
+        {
+            case 1:
+                movement = Input.GetAxis("MovePlayer1");
+                break;
+            case 2:
+                movement = Input.GetAxis("MovePlayer2");
+                break;
+        }
+
+
+        return movement;
+    }
+    private void Move(float movement)
+    {
+        Vector2 velo = rb2d.velocity;
+        velo.y= movement * moveSpeed;
+        rb2d.velocity = velo;
+
+    }
+
+    
 }
